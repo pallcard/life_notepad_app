31ccecd85296a986e08027a388fe666e71d5bf11
liuke <1250585541@qq.com>
2023-08-08 02:14:10 +0800

    feat: home静态页面

---
+++ lib/page/component/NotepadCard.dart
@@ -0,0 +1,74 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotepadCard extends StatelessWidget {
+
+
+  late String avatar;
+  late String nickName;
+  late String createTime;
+  late String content;
+  late List<String> images;
+  late String location;
+
+
+  @override
+  Widget build(BuildContext context) {
+    images = ["https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1c5a5c88-3063-4615-905a-a9b9e4c2acb5%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694020103&t=15637d7ccac5a81aa1e0fa4a558efed9",
+      "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1c5a5c88-3063-4615-905a-a9b9e4c2acb5%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694020103&t=15637d7ccac5a81aa1e0fa4a558efed9",
+      "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1c5a5c88-3063-4615-905a-a9b9e4c2acb5%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694020103&t=15637d7ccac5a81aa1e0fa4a558efed9",
+      "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1c5a5c88-3063-4615-905a-a9b9e4c2acb5%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694020103&t=15637d7ccac5a81aa1e0fa4a558efed9",
+      "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1c5a5c88-3063-4615-905a-a9b9e4c2acb5%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694020103&t=15637d7ccac5a81aa1e0fa4a558efed9",
+      "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1c5a5c88-3063-4615-905a-a9b9e4c2acb5%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694020103&t=15637d7ccac5a81aa1e0fa4a558efed9"];
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Card(child: Column(children: [
+        Row(children: [
+          Container(
+            width: 50,
+            height: 50,
+            padding: const EdgeInsets.all(8),
+            child: const Image(image: NetworkImage('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1c5a5c88-3063-4615-905a-a9b9e4c2acb5%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694020103&t=15637d7ccac5a81aa1e0fa4a558efed9'),),
+          ),
+          const Text("用爱发电的小程序开发者",style:TextStyle(fontSize: 12,),),
+        ],),
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.all(8),
+          child: const Text("对研究经济学的学者来讲，2003年以来中国经济出现了许多不容易  理解的现象。从2003年开始中国的经济连续4年平均每年的增长",style:TextStyle(fontSize: 15,),),
+        ),
+        Container(
+          child: GridView.builder(
+              shrinkWrap:true,
+              physics:const NeverScrollableScrollPhysics(),
+              itemCount:images.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount:3,
+                mainAxisSpacing:3,
+                  childAspectRatio: 1.0
+              ),
+              itemBuilder: (BuildContext context, int position ) {
+                return 
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    child:  Image(image: NetworkImage('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1c5a5c88-3063-4615-905a-a9b9e4c2acb5%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694020103&t=15637d7ccac5a81aa1e0fa4a558efed9'),),
+                  );
+              }),
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.only(left: 8),
+          child: const Text("2022-11-11 11:11:11",style:TextStyle(fontSize: 10,),),
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.only(left: 8, bottom: 8),
+          child: const Text("湖北武汉",style:TextStyle(fontSize: 10,),),
+        ),
+      ],)),
+    );
+  }
+
+}
--- lib/page/nav/home.dart
+++ lib/page/nav/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:life_notepad_app/page/component/NotepadCard.dart';
 
 class HomaPage extends StatefulWidget {
   const HomaPage({super.key});
@@ -10,6 +12,15 @@ class HomaPage extends StatefulWidget {
 class _HomaPageState extends State<HomaPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("home"),);
+    return Stack(
+      children: [
+    ListView.builder(
+    itemBuilder: (BuildContext context, int i) {
+      return NotepadCard();
+    }),
+        FloatingActionButton( child: const Icon(Icons.add),onPressed: () { print("ssss"); },)
+      ],
+    );
+
   }
 }
