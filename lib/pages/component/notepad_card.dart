9ac44c24e5f21b773c234817822beff90e44f744
liuke <1250585541@qq.com>
2023-08-14 02:34:42 +0800

    feat: 调通数据

--- lib/pages/component/notepad_card.dart
+++ lib/pages/component/notepad_card.dart
@@ -12,73 +12,78 @@ class NotepadCard extends StatelessWidget {
       margin: const EdgeInsets.only(bottom: 10),
       child: Card(
           child: Column(
-        children: [
-          Row(
             children: [
+              Row(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    padding: const EdgeInsets.all(8),
+                    child: Image(
+                      image: NetworkImage(
+                        note.avatar ?? "",
+                      ),
+                    ),
+                  ),
+                  Text(
+                    note.nickName ?? "",
+                    style: const TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
               Container(
-                width: 50,
-                height: 50,
+                alignment: Alignment.centerLeft,
                 padding: const EdgeInsets.all(8),
-                child: Image(
-                  image: NetworkImage(
-                    note.avatar ?? "",
+                child: Text(
+                  note.content ?? "",
+                  style: const TextStyle(
+                    fontSize: 15,
                   ),
                 ),
               ),
-              Text(
-                note.nickName ?? "",
-                style: const TextStyle(
-                  fontSize: 12,
+              (note.images == null || note.images!.isNotEmpty)
+                  ? const Text("")
+                  :
+              GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: note.images?.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 3,
+                      childAspectRatio: 1.0),
+                  itemBuilder: (BuildContext context, int position) {
+                    return Container(
+                      padding: EdgeInsets.all(8),
+                      child: Image(
+                        image: NetworkImage(note.images![position]),
+                      ),
+                    );
+                  }),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 8),
+                child: Text(
+                  note.createTime ?? "",
+                  style: const TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 8, bottom: 8),
+                child: Text(
+                  note.location ?? "",
+                  style: const TextStyle(
+                    fontSize: 10,
+                  ),
                 ),
               ),
             ],
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              note.content ?? "",
-              style: const TextStyle(
-                fontSize: 15,
-              ),
-            ),
-          ),
-          GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: note.images?.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, mainAxisSpacing: 3, childAspectRatio: 1.0),
-              itemBuilder: (BuildContext context, int position) {
-                return Container(
-                  padding: EdgeInsets.all(8),
-                  child: Image(
-                    image: NetworkImage(note.images![position]),
-                  ),
-                );
-              }),
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.only(left: 8),
-            child: Text(
-              note.createTime ?? "",
-              style: const TextStyle(
-                fontSize: 10,
-              ),
-            ),
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.only(left: 8, bottom: 8),
-            child: Text(
-              note.location ?? "",
-              style: const TextStyle(
-                fontSize: 10,
-              ),
-            ),
-          ),
-        ],
-      )),
+          )),
     );
   }
 }
